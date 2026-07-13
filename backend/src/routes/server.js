03c5ff76da7e9520f9b7df2d228b0b3dbe4eaff2
Laura <laurinha.ss1305@gmail.com>
2026-07-13 16:35:58 -0300

    Delete backend directory

--- backend/src/routes/server.js
+++
@@ -1,9 +0,0 @@
-// src/server.js
-const app = require('./app');
-require('dotenv').config();
-
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`Servidor ativo e sincronizado com o PostgreSQL na porta ${PORT}`);
-});
